ff9985a47e4e37323137921b04a50619d47016ca
Christofer Perez <christoferperez@gmail.com>
2019-08-22 15:35:55 +0200

    frontEnd update

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,9 +4,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>CatMash</div>
-      </header>
+      <table className="titleBar">
+        <tbody>
+          <td>
+            <img
+              alt="app icon"
+              width="80"
+              src="57301f8bf69d246fceaff705bd511fa4.jpg"
+            />
+          </td>
+          <td width="350" />
+          <td>
+            <h1>CatMash</h1>
+          </td>
+        </tbody>
+      </table>
+
+      <div>
+        <h2>Choose this one</h2>
+      </div>
+      <div>
+        <h2>Maybe this one is the cuttiest one</h2>
+      </div>
     </div>
   );
 }
